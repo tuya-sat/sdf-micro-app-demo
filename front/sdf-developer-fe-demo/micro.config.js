c0dd81d15a380d37f4272eb71c3e42b32abf8eb5
qingjiu <qingjiu.wang@tuya.com>
2022-01-13 13:34:54 +0800

    chore: 更新前端demo模板

--- front/sdf-developer-fe-demo/micro.config.js
+++ front/sdf-developer-fe-demo/micro.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   debuggerConfig: {
     target: 'http://mysaas.com:3000',
-    username: 'hello@mysaas.com',
-    password: 'Admin123456',
+    username: 'xxx', // 超级管理员账号 
+    password: 'xxx', // 超级管理员密码
     logSign: true,
+    needProxyApi: ["/api", "/open-api", "/custom-api"],
   },
 };
