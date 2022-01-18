fd2ed9e02b82655b8e75fa7ac9cbc8577f8d1694
qingjiu <qingjiu.wang@tuya.com>
2022-01-18 15:10:27 +0800

    fix：安全检查

--- front/sdf-developer-fe-demo/micro.config.js
+++ front/sdf-developer-fe-demo/micro.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   debuggerConfig: {
-    target: 'xxx',
-    username: 'xxx', // 超级管理员账号 
-    password: 'xxx', // 超级管理员密码
+    target: "xxx", // 需要代理的host
+    username: "xxx", // 超级管理员账号
+    password: "xxx", // 超级管理员密码
     logSign: true,
     needProxyApi: ["/api", "/open-api", "/custom-api"],
   },
